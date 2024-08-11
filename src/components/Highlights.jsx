f30dc376a69a11ebf56d4b45a5281115a2058a66
haticebabaoglu <haticebaba63@gmail.com>
2024-08-11 23:57:59 +0300

    .

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -14,7 +14,7 @@ const Highlights = () => {
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
-          <h1 id="title" className="section-heading">Get  the highlights.</h1>
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
 
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
