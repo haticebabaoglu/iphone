1f0c898997f7a3d65f361f9e5e5a35029acfc9b2
haticebabaoglu <haticebaba63@gmail.com>
2024-08-12 23:45:57 +0300

    .

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -19,7 +19,7 @@ const Highlights = () => {
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
               Watch the film
-              <img src={watchImg} alt="watch" className="ml-" />
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
             <p className="link">
               Watch the event
